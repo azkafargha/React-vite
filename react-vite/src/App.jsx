c1a721a70be529cf2117abc47b25baf141be9699
AzkaFargh <azka.farghani.2001@gmail.com>
2024-12-09 09:00:48 +0700

    nyoba euy

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import bsiLogo from './assets/images.png'
+import Logo from './components/logo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,14 +11,9 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Logo/>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Anies + Ahok</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -25,6 +22,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
